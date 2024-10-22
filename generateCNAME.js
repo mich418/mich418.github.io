877071d766cc77f7c333d46bb228466541e5883e
mihauco <github@mihau.co>
2024-10-22 19:49:29 +0200

    v1.5.0 - change domain name

--- generateCNAME.js
+++ generateCNAME.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 if (fs.existsSync('./dist')) {
-  fs.appendFileSync('./dist/CNAME', 'mihau.co', {flag: 'w'})
+  fs.appendFileSync('./dist/CNAME', 'michal.dev', {flag: 'w'})
 } else {
   throw new Error('Could not create CNAME file - dist dir does not exist.')
 }
