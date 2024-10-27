b0287f463ea45ff0d584b27965a84602a5e4ceba
mich418 <github@michal.dev>
2024-10-27 12:36:06 +0100

    fix routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,33 @@ import { navigation } from '@/variables/variables'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: navigation.map((route) => ({
-    name: route.name,
-    path: route.path,
-    component: () =>  import(/* @vite-ignore */ `../views/${route.componentName}`)
-  }))
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () =>  import('@/views/HomeView.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () =>  import('@/views/AboutView.vue'),
+    },
+    {
+      path: '/texts',
+      name: 'texts',
+      component: () =>  import('@/views/TextsView.vue'),
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () =>  import('@/views/ContactView.vue'),
+    },
+    {
+      path: '/privacy',
+      name: 'privacy',
+      component: () =>  import('@/views/PrivacyView.vue'),
+    }
+  ]
 })
 
 export default router
